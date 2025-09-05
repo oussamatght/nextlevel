eb4d246ba7456d9cb43b2f366011b1cf9321c4eb
oussama <oussamatght6@gmail.com>
2025-09-06 01:40:49 +0200

    Update order.js

--- app/_util/order.js
+++ app/_util/order.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:1337/api/orders";
+const API_URL = "https://strip-1.onrender.com/api/orders";
 const creatorder = (payload) => axios.post(`${API_URL}`, payload);
 export default creatorder;
