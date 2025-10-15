bc92735fec8648ee0482af226bb91ba554bffdd3
oussama <oussamatght6@gmail.com>
2025-10-15 16:33:58 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@
 
 import Hero from "./hero/page.jsx";
 import Product from "./product/page.jsx";
-import Testimonials from "./components/Testimonials.jsx";
+import Testimonials from "../components/Testimonials.jsx";
 
 export default function Home() {
   return (
