81f7286de45d3080aaa6f097e048ca5f33a6b618
oussama <oussamatght6@gmail.com>
2025-10-15 17:17:38 +0200

    Update page.jsx

--- app/here/[productid]/_componenent/productbanner/page.jsx
+++ app/here/[productid]/_componenent/productbanner/page.jsx
@@ -1,10 +1,16 @@
+"use client";
+
 export default function ProductBanner({ pro }) {
   return (
-    <div>
-     {
-    pro.banner.map((img, index) => (
-      <img key={index} src={img.url} alt={img.alt} className='w-[500px] h-[300px]  rounded-lg ' />
-   ))}
+    <div className="relative w-full">
+      {pro?.banner?.map((img, index) => (
+        <img
+          key={index}
+          src={img.url}
+          alt={img.alt || pro.title}
+          className="w-full h-[300px] sm:h-[400px] object-cover rounded-xl shadow-lg hover:scale-[1.02] transition-transform duration-500"
+        />
+      ))}
     </div>
   );
 }
